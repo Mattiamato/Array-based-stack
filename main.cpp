0ebea42723fc79026619efb3d01c68255f9858ff
Mattiamato <mattiamato@gmail.com>
2012-03-05 21:02:03 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  stack_arraybased
+//
+//  Created by Loc Nguyen on 08.02.12.
+//  Copyright (c) 2012 Universität Zürich. All rights reserved.
+//
+
+#include <iostream>
+#include "stack_ab.h"
+
+int main (int argc, const char * argv[])
+{
+
+    Stack_ab mystack;
+    std::string temp;
+    std::cout<<"initial size of stack: "<<mystack.size()<<"\n";
+    try {
+        mystack.push("Villa");
+        mystack.push("Messi");
+        mystack.push("Alexis");
+        mystack.push("Iniesta");
+        mystack.push("Xavi");
+        mystack.push("Mascherano");
+        mystack.push("Dani Alves");
+        mystack.push("Abidal");
+        mystack.push("Pique");
+        mystack.push("Puyol");
+        mystack.push("Valdes");
+
+    } catch (OverFlow& e) {
+        e.overflowoutput();
+    }
+        
+    std::cout<<"current size of stack: "<<mystack.size()<<"\n";
+
+    std::cout<<"current top element is: "<<mystack.top()<<"\n";
+    
+    try {
+        while (!mystack.empty()) {
+            std::cout<<mystack.pop()<<" wurde gepopt\n";
+            std::cout<<mystack.top()<<" is now on top\n";
+            //in the console these sentences are funny =)
+        }
+    } catch (UnderFlow& e) {
+        e.underflowoutput();  
+    }
+    
+    
+
+    return 0;
+}
+
---
+++ stack_ab.cpp
@@ -0,0 +1,14 @@
+//
+//  stack_ab.cpp
+//  stack_arraybased
+//
+//  Created by Loc Nguyen on 16.02.12.
+//  Copyright (c) 2012 Universität Zürich. All rights reserved.
+//
+
+#include <iostream>
+#include "stack_ab.h"
+
+//all implementations here!!!
+
+
