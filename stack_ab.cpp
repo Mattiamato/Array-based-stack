878deb6a79306f60c48b711a972dfa9c3ad1f9b8
Mattiamato <mattiamato@gmail.com>
2012-03-05 21:12:08 +0100

    method definition updated

--- stack_ab.cpp
+++ stack_ab.cpp
@@ -11,4 +11,37 @@
 
 //all implementations here!!!
 
+Stack_ab::Stack_ab()	//constructor
+{
 
+}
+
+Stack_ab::~Stack_ab()	//destructor
+{
+
+}
+
+int Stack_ab::size()	//return stack size
+{
+
+}
+
+void Stack_ab::push(std:: string)	//add to stack
+{
+
+}
+
+std::string Stack_ab::pop()			//remove from stack
+{
+
+}
+
+std::string Stack_ab::top()			//return top position
+{
+
+}
+
+bool Stack_ab::empty()				//is_empty
+{
+
+}
